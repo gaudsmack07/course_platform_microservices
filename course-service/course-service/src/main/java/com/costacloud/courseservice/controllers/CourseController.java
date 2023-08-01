bb1dcb70af133df30b39ee0ae646cd22ea356c71
vishal <gaudsmack07@gmail.com>
2023-08-01 18:09:25 +0530

    All microservices configured to log to Fluentd

--- course-service/course-service/src/main/java/com/costacloud/courseservice/controllers/CourseController.java
+++ course-service/course-service/src/main/java/com/costacloud/courseservice/controllers/CourseController.java
@@ -19,6 +19,9 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/course")
