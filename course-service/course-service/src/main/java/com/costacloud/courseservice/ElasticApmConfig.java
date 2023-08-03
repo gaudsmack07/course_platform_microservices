06dfd78fbedbf26454f2a99402f9b2b9af6a835b
vishal <gaudsmack07@gmail.com>
2023-08-03 11:31:29 +0530

    Elastic APM integration added for monitoring

---
+++ course-service/course-service/src/main/java/com/costacloud/courseservice/ElasticApmConfig.java
@@ -0,0 +1,38 @@
+package com.costacloud.courseservice;
+
+import co.elastic.apm.attach.ElasticApmAttacher;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "elastic.apm")
+@ConditionalOnProperty(value = "elastic.apm.enabled", havingValue = "true")
+public class ElasticApmConfig {
+
+    private static final String SERVER_URL_KEY = "server_url";
+    private String serverUrl;
+
+    private static final String SERVICE_NAME_KEY = "service_name";
+    private String serviceName;
+
+    private static final String APPLICATION_PACKAGES_KEY = "application_packages";
+    private String applicationPackages;
+
+    @PostConstruct
+    public void init() {
+
+    	Map<String, String> apmProps = new HashMap<>(6);
+        apmProps.put(SERVER_URL_KEY, serverUrl);
+        apmProps.put(SERVICE_NAME_KEY, serviceName);
+        apmProps.put(APPLICATION_PACKAGES_KEY, applicationPackages);
+
+        ElasticApmAttacher.attach(apmProps);
+    }
+}
